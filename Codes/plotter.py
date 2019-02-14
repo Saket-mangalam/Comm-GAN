fa60530f286765ae54808244e55b5ca42c2f9efc
Saket Mangalam <saketm@uw.edu>
2019-02-13 20:21:00 -0800

    updated plotting

--- Codes/plotter.py
+++ Codes/plotter.py
@@ -8,34 +8,113 @@ Created on Fri Nov 30 08:29:55 2018
 import csv
 import matplotlib.pyplot as plt
 #import numpy as np
-import re
+#import re
 
-with open('logbook/262468.csv') as csv_file:
+with open('logbook/430465.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    g = []
-    d= [] 
-    dec = []
-    ber=[]
+    g1 = []
+    d1 = []
+    dec1 = []
+    ber1 = []
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            print('Column names are {", ".join(row)}')
             line_count += 1
         else:
-            g.append(float(row[1]))
-            d.append(float(row[2]))
-            dec.append(float(row[3]))
-            ber.append(float(re.findall(r"[-+]?\d*\.\d+|\d+", row[4])[0]))
+            g1.append(float(row[2])+float(row[4]))
+            d1.append(float(row[1]))
+            dec1.append(float(row[3]))
+            ber1.append(float(row[5]))
             line_count += 1
-print(f'Processed {line_count} lines.')
+print('Processed {line_count} lines.')
+
+with open('logbook/106568.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    g2 = []
+    d2 = []
+    dec2 = []
+    ber2 = []
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            g2.append(float(row[2])+float(row[4]))
+            d2.append(float(row[1]))
+            dec2.append(float(row[3]))
+            ber2.append(float(row[5]))
+            line_count += 1
+print('Processed {line_count} lines.')
+
+with open('logbook/567447.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    g3 = []
+    d3 = []
+    dec3 = []
+    ber3 = []
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            g3.append(float(row[2])+float(row[4]))
+            d3.append(float(row[1]))
+            dec3.append(float(row[3]))
+            ber3.append(float(row[5]))
+            line_count += 1
+print('Processed {line_count} lines.')
+
+with open('logbook/500595.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    g4 = []
+    d4 = []
+    dec4 = []
+    ber4 = []
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            g4.append(float(row[2])+float(row[4]))
+            d4.append(float(row[1]))
+            dec4.append(float(row[3]))
+            ber4.append(float(row[5]))
+            line_count += 1
+print('Processed {line_count} lines.')
+
+with open('logbook/854309.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    g5 = []
+    d5 = []
+    dec5 = []
+    ber5 = []
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            g5.append(float(row[2])+float(row[4]))
+            d5.append(float(row[1]))
+            dec5.append(float(row[3]))
+            ber5.append(float(row[5]))
+            line_count += 1
+print('Processed {line_count} lines.')
 
 fig = plt.figure()
 ax = plt.axes()
 
-x = list(range(len(g)))
-ax.plot(x,g)
-ax.plot(x,d)
-ax.plot(x,dec)
-ax.plot(x,ber)
-ax.legend(['gloss','dloss','decloss','ber'])
-ax.set(title="262468", xlabel="batches done", ylabel="losses")
+x = list(range(len(g1)))
+ax.plot(x,ber1)
+#ax.plot(x,ber2)
+#ax.plot(x,ber3)
+#ax.plot(x,ber4)
+#ax.plot(x,ber5)
+#ax.legend(['Noise 0','Noise 0.1','Noise 0.3','Noise 0.5','Noise 0.8'])
+ax.set(title="Disc:dec 1:999", xlabel="batches done", ylabel="losses")
+
+plt.show()
