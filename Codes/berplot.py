fa23f665963835cdfab7fdf0915518837e51b100
Saket Mangalam <saketm@uw.edu>
2019-02-13 20:21:28 -0800

    added snr testing

---
+++ Codes/berplot.py
@@ -0,0 +1,62 @@
+import csv
+import matplotlib.pyplot as plt
+
+with open('logbook/test_ber/250788.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    loss0 = []
+    for row in csv_reader:
+        loss0.append(float(row[1]))
+        line_count += 1
+print('Processed {line_count} lines.')
+
+
+with open('logbook/test_ber/106568.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    loss1 = []
+    for row in csv_reader:
+        loss1.append(float(row[1]))
+        line_count += 1
+print('Processed {line_count} lines.')
+
+with open('logbook/test_ber/567447.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    loss3 = []
+    for row in csv_reader:
+        loss3.append(float(row[1]))
+        line_count += 1
+print('Processed {line_count} lines.')
+
+with open('logbook/test_ber/500595.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    loss5 = []
+    for row in csv_reader:
+        loss5.append(float(row[1]))
+        line_count += 1
+print('Processed {line_count} lines.')
+
+with open('logbook/test_ber/854309.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    loss8 = []
+    for row in csv_reader:
+        loss8.append(float(row[1]))
+        line_count += 1
+print('Processed {line_count} lines.')
+
+fig = plt.figure()
+ax = plt.axes()
+
+x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+ax.plot(x,loss0)
+ax.plot(x,loss1)
+ax.plot(x,loss3)
+ax.plot(x,loss5)
+ax.plot(x,loss8)
+ax.legend(['Noise 0','Noise 0.1','Noise 0.3','Noise 0.5','Noise 0.8'])
+ax.set(title="Testing of different trainings", xlabel="SNR", ylabel="BER")
+
+plt.show()
