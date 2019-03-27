cb0909c5c736fe2c5fa5adfe301b598b3127bed4
Saket Mangalam <saketm@uw.edu>
2019-03-27 11:39:08 -0700

    Add files via upload

--- EncoGAN/Encoders.py
+++ EncoGAN/Encoders.py
@@ -3,9 +3,9 @@ import torch
 
 
 
-class Encoder(nn.Module):
+class Basic_Encoder(nn.Module):
     def __init__(self, args):
-        super(Encoder, self).__init__()
+        super(Basic_Encoder, self).__init__()
         self.args = args
         self.nc = self.args.img_channels
         self.nef = self.args.enc_lat
@@ -18,12 +18,12 @@ class Encoder(nn.Module):
             # input is (nc) x 64 x 64
             nn.Conv2d(self.nc, self.nef, 3, 1, 1, bias=False),
             nn.BatchNorm2d(self.nef),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2, inplace=True)
         )
 
         self.encode = nn.Sequential(
-            #input is (nef+ 3*ud) x 64 x 64
-            nn.Conv2d((self.nef + self.nc * self.ud), self.nef, 3, 1, 1, bias=False),
+            #input is (nef+ ud) x 64 x 64
+            nn.Conv2d((self.nef + self.ud), self.nef, 3, 1, 1, bias=False),
             nn.BatchNorm2d(self.nef),
             nn.LeakyReLU(0.2, inplace= True),
             # input is nef x 64 x64
@@ -37,8 +37,112 @@ class Encoder(nn.Module):
         )
 
     def forward(self, img, u):
-        eimg = self.enc_Img(img)
-        eimg = torch.cat([eimg,u], dim=1)
-        output = self.encode(eimg)
+        output = self.enc_Img(img)
+        output = torch.cat([output,u], dim=1)
+        output = self.encode(output)
         return output
+
+
+class Residual_Encoder(nn.Module):
+    def __init__(self, args):
+        super(Residual_Encoder, self).__init__()
+        self.args = args
+        self.nc = self.args.img_channels
+        self.nef = self.args.enc_lat
+        self.ud = self.args.bitsperpix
+
+        cuda = True if torch.cuda.is_available() else False
+        self.this_device = torch.device("cuda" if cuda else "cpu")
+
+        self.enc_Img = nn.Sequential(
+            #input is nc X 64X64
+            nn.Conv2d(self.nc,self.nef, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True)
+
+        )
+
+        self.encode = nn.Sequential(
+            #input is nef + ud X64X64
+            nn.Conv2d((self.nef + self.ud), self.nef, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True),
+            #input is nef x 64 x64
+            nn.Conv2d(self.nef, self.nef, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True),
+            #input is nef X 64x64
+            nn.Conv2d((self.nef+self.nc),self.nc, 3, 1, 1, bias=False)
+        )
+
+        self.tanh = nn.Sequential(
+            nn.Tanh()
+            # output image
+        )
+
+    def forward(self, img, u):
+        output = self.enc_Img(img)
+        output = torch.cat([output,u], dim=1)
+        output = self.encode(output)
+        output += img
+        output = self.tanh(output)
+        return output
+
+
+class Dense_Encoder(nn.Module):
+    def __init__(self, args):
+        super(Dense_Encoder, self).__init__()
+        self.args = args
+        self.nc = self.args.img_channels
+        self.nef = self.args.enc_lat
+        self.ud = self.args.bitsperpix
+
+        cuda = True if torch.cuda.is_available() else False
+        self.this_device = torch.device("cuda" if cuda else "cpu")
+
+        self.enc_Img = nn.Sequential(
+            #state size nc X 64 x64
+            nn.Conv2d(self.nc, self.nef, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        self.encode1 = nn.Sequential(
+            #statesize is nef+_ud X 64 x64
+            nn.Conv2d((self.nef+self.ud), self.nef, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        self.encode2 = nn.Sequential(
+            #statesize is 2*nef + ud X 64 x64
+            nn.Conv2d((2*self.nef + self.ud), self.nef, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        self.encode3 = nn.Sequential(
+            #state size is 3*nef + ud X 64 x64
+            nn.Conv2d((3*self.nef + self.ud), self.nef, 3, 1, 1, bias=False),
+            nn.batchNorm2d(self.nef),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+        self.tanh = nn.Sequential(
+            nn.Tanh()
+            # output image
+        )
+
+    def forward(self, img, u):
+        output1 = self.enc_Img(img)
+        output2 = torch.cat([output1,u], dim=1)
+        output2 = self.encode1(output2)
+        output3 = torch.cat([output1,output2,u], dim=1)
+        output3 = self.encode2(output3)
+        output4 = torch.cat([output1, output2, output3, u], dim=1)
+        output4 = self.encode3(output4)
+        output4 += img
+        output4 = self.tanh(output4)
+        return output4
+    
 
